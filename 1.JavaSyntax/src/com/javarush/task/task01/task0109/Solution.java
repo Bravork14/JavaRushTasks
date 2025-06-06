49a9206b1a49bc115bda323c7d8b3f5168f1c32b
Vadim Panteleev <crumbs07@mail.ru>
2025-06-06 10:23:47 +0300

    Добавил gitignore

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+Объявление и инициализация переменных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+    }
+}
