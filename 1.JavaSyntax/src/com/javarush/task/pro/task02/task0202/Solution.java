d3c029e06852850def02aad3226847bb2b8187d3
Vadim Panteleev <crumbs07@mail.ru>
2025-06-06 10:33:07 +0300

    Добавил gitignore

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         int centimeters = 243;
 
-        int meters = 0; //напишите тут ваш код
+        int meters = centimeters/100;
 
         System.out.println(meters);
     }
