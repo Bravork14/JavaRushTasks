19ddb98e2a2fdb263fa952d168c29ddf8f7451dd
Vadim Panteleev <crumbs07@mail.ru>
2025-06-23 22:05:05 +0300

    Добавил task0410

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -8,6 +8,28 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int min = console.nextInt();
+        int secondMin = console.nextInt();
+        if (min > secondMin) {
+            int temp = min;
+            min = secondMin;
+            secondMin = temp;
+        }
+        while (console.hasNextInt()) {
+            int number = console.nextInt();
+            if (min == secondMin && number > min) {
+                secondMin = number;
+            } else if (number > min && secondMin > number) {
+                secondMin = number;
+            } else if (number < min) {
+                secondMin = min;
+                min = number;
+            }
 
+        }
+
+
+        System.out.println(secondMin);
     }
 }
