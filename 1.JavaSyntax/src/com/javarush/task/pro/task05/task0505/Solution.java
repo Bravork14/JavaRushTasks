b789f0abf55bb91f4c546b60cf24f27f08fc8f0b
Vadim Panteleev <crumbs07@mail.ru>
2025-07-06 18:24:54 +0300

    task0504

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -8,6 +8,20 @@ Reverse
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int num = console.nextInt();
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = console.nextInt();
+        }
+        if (num % 2 != 0) {
+            for (int i = 0; i < num; i++) {
+                System.out.println(array[i]);
+            }
+        } else {
+            for (int i = array.length - 1; i >= 0; i--) {
+                System.out.println(array[i]);
+            }
+        }
     }
 }
