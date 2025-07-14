3ee43fbdaa1231296a46537f5f3173cc7d2c4562
Vadim Panteleev <crumbs07@mail.ru>
2025-07-14 14:31:28 +0300

    добавил task0509;

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -10,14 +10,12 @@ public class Solution {
 
     public static void main(String[] args) {
         MULTIPLICATION_TABLE = new int[10][10];
-        for(int i = 0; i < MULTIPLICATION_TABLE.length;i++){
-            for (int j = 0; j < MULTIPLICATION_TABLE[i].length;j++){
-                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
+            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
+                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1 );
                 System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
             }
             System.out.println();
-
         }
-
     }
 }
