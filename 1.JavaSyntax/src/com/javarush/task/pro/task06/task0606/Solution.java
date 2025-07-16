bcfcbed6078fd9bb148daaea2ae1df4e8856a99c
Vadim Panteleev <crumbs07@mail.ru>
2025-07-16 15:37:21 +0300

    добавил task0606;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(String text) {
+
+    }
+
+    public static void universalMethod(int val) {
+
+    }
+
+    public static void universalMethod(double a) {
+
+    }
+
+    public static void universalMethod(boolean a) {
+
+    }
+
+    public static void universalMethod(int[] data, int value) {
+
+    }
+
+    public static void universalMethod(int[][] data, int value) {
+
+    }
+
+    public static void universalMethod(int[][][] data, int value) {
+
+    }
+
+    public static void universalMethod(String str, String str2) {
+
+    }
+
+    public static void universalMethod(double a, double b) {
+
+    }
+
+    public static void universalMethod(long a) {
+
+    }
+    //напишите тут ваш код
+}
