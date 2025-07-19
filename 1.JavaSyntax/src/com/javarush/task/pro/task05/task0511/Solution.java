bdb366ce708f71e92971d4282f2b3249d996f68a
Vadim Panteleev <crumbs07@mail.ru>
2025-07-19 23:06:16 +0300

    добавил task0511;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task05.task0511;
+
+import java.util.Scanner;
+
+/* 
+Создаем двумерный массив
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+        multiArray = new int[N][];
+        for (int i = 0; i < N; i++) {
+            int length = console.nextInt();
+            multiArray[i] = new int[length];
+        }
+        for (int i = 0; i < multiArray.length ; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                System.out.print("[]");
+            }
+            System.out.println();
+        }
+    }
+}
