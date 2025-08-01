758a7cba4feb566ff5483520c1241da5c2fb8f36
Vadim Panteleev <crumbs07@mail.ru>
2025-08-02 00:55:31 +0300

    добавил task0703;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte one = -128;
+    short two = -32_768;
+    int three = -2_147_483648;
+    long five = Long.MIN_VALUE;
+}
