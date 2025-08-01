97ff8274048bea0159d31fa657e9cec0a954fe0d
Vadim Panteleev <crumbs07@mail.ru>
2025-08-02 00:52:05 +0300

    добавил task0702;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte one = 127;
+    short two = 32767;
+    int three = 2_147_483647;
+    long four = Long.MAX_VALUE;
+}
