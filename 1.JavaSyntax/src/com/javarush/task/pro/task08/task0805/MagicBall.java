123e898e0e00b11642987faa76c232a4548158fa
Vadim Panteleev <crumbs07@mail.ru>
2025-08-01 15:49:22 +0300

    добавил task0616;

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/MagicBall.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/MagicBall.java
@@ -13,26 +13,7 @@ public class MagicBall {
     private static final String VERY_DOUBTFUL = "Весьма сомнительно";
 
     public static String getPrediction() {
-        Random random = new Random();
-        int number = random.nextInt(8);
-        if (number == 0) {
-            return CERTAIN;
-        } else if (number == 1) {
-            return DEFINITELY;
-        } else if (number == 2) {
-            return MOST_LIKELY;
-        } else if (number == 3) {
-            return OUTLOOK_GOOD;
-        } else if (number == 4) {
-            return ASK_AGAIN_LATER;
-        } else if (number == 5) {
-            return TRY_AGAIN;
-        } else if (number == 6) {
-            return NO;
-        } else if (number == 7) {
-            return VERY_DOUBTFUL;
-        } else {
-            return null;
-        }
+        //напишите тут ваш код
+        return null;
     }
 }
