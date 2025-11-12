3dc0a1866ce8ea84f80ea5cb1a2b09d74afbb145
Vadim Panteleev <crumbs07@mail.ru>
2025-11-13 01:04:30 +0300

    добавил task1020;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task10.task1020;
+
+import java.security.Key;
+
+public class Computer {
+    public Computer() {
+        new SystemUnit();
+        new Monitor();
+        new Keyboard();
+        new Mouse();
+    }
+}
